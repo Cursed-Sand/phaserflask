818cc257ffb85ed256aaba04d28a23d005bd2ec5
Travis Turk <travisturk@gmail.com>
2021-06-09 23:40:57 -0500

    began db setup

--- application.py
+++ application.py
@@ -17,9 +17,52 @@ db = SQL(os.getenv('DATABASE_URL'))
 def index():
     return render_template('index.html')
 
+
+@app.route("/test", methods=['GET'])
+def test():
+
+    # CREATE TABLES
+    # Users
+    db.execute("CREATE TABLE IF NOT EXISTS users ( \
+        id serial PRIMARY KEY NOT NULL, \
+        username VARCHAR ( 255 ) UNIQUE NOT NULL, \
+        password VARCHAR ( 255 ) NOT NULL, \
+        created_on TIMESTAMP, \
+        last_login TIMESTAMP \
+        )")
+
+    # Characters
+        # id
+        # name
+        # description
+        #         
+
+    # Classes
+        # id
+        # name
+        # description
+        # cantrip
+
+
+    # Abilities (52+2)
+        # id
+        # name
+        # description
+        # pulled/drawn        
+
+
+    return 'HELLO TEST'
+
+@app.route("/db", methods=['GET'])
+def db():
+    
+    return 'HELLO db'
+
+
+
+
 if __name__ == "__main__":
     FLASK_DEBUG=1
     server = Server(app.wsgi_app)
     server.serve()
-
 
