80e5bcf5a03b183fd3b999ace60b058dddc3a362
Travis Turk <travisturk@gmail.com>
2021-06-09 21:46:53 -0500

    connected to postgres

--- application.py
+++ application.py
@@ -1,14 +1,22 @@
+import os
+
 from flask import Flask, render_template
 from livereload import Server
+from cs50 import SQL
 
 app = Flask(__name__)
 app.config.from_pyfile('settings/development.conf')
+
+# Configure Heroku Postgres database
+db = SQL(os.getenv('DATABASE_URL'))
+
 
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
+    FLASK_DEBUG=1
     server = Server(app.wsgi_app)
     server.serve()
 
