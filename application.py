7b76059230a547ac50df7c6ddf92b9163dbe21d9
mike <rmchurch88@gmail.com>
2021-06-17 17:47:28 -0500

    same as before but this time the style.css file is in the right place

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 import os
-import psycopg2
+# import psycopg2
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session, url_for, flash
 from livereload import Server
 from cs50 import SQL
 
 app = Flask(__name__)
 app.config.from_pyfile('settings/development.conf')
-
+app.secret_key = os.getenv('SECRET_KEY')
 
 # Configure Heroku Postgres database
 db = SQL(os.getenv('DATABASE_URL'))
@@ -60,9 +60,112 @@ def db():
 
 
 
+# ADMINISTRATIVE #
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # Serve registration page
+    if request.method == 'GET':
+        return render_template("register.html")
+
+    # Process submitted form responses on POST
+    else:
+
+        # Error Checking
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", errcode=403, errmsg="Username required.")
+
+        # Ensure password was submitted
+        if not request.form.get("password"):
+            return render_template("error.html", errcode=403, errmsg="Password required.")
+
+        # Ensure password and password confirmation match
+        if request.form.get("password") != request.form.get("passwordconfirm"):
+            return render_template("error.html", errcode=403, errmsg="Passwords must match.")
+
+        # Ensure minimum password length
+        if len(request.form.get("password")) < 8:
+            return render_template("error.html", errcode=403, errmsg="Password must be at least 8 characters.")
+
+        # Store the hashed username and password
+        username = request.form.get("username")
+        hashedpass = generate_password_hash(request.form.get("password"))
+
+        if username not in authusers:
+            return render_template("error.html", errcode=403, errmsg="Unauthorized user.")
+
+        # Check if username is already taken
+        if not db.execute("SELECT username FROM users WHERE username LIKE (?)", username):
+
+            # Add the username
+            time = datetime.datetime.utcnow().isoformat()
+            db.execute("INSERT INTO users (username, password, created_on) VALUES (:username, :hashedpass, :time)",
+                        username=username, hashedpass=hashedpass, time=time)
+            return redirect("/")
+
+        else:
+            return render_template("error.html", errcode=403, errmsg="Username invalid or already taken.")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", errcode=400, errmsg="Username required.")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html", errcode=400, errmsg="Password required.")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists
+        if len(rows) != 1:
+            return render_template("register.html", errmsg="Username not found.")
+
+        # Ensure username exists and password is correct
+        if not check_password_hash(rows[0]["password"], request.form.get("password")):
+            return render_template("error.html", errcode=403, errmsg="Incorrect password.")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Update "last_login"
+        time = datetime.datetime.utcnow().isoformat()
+        db.execute("UPDATE users SET last_login=:time WHERE id=:id", time=time, id=session["user_id"])
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+   # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
 
 if __name__ == "__main__":
-    FLASK_DEBUG=1
     server = Server(app.wsgi_app)
     server.serve()
 
